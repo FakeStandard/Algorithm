15de64ac6a20cf73c8f0b52282943ea4c631b9b1
FakeStandard <rileylin.space@gmail.com>
2021-05-25 10:25:20 +0800

    Initial commit

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
