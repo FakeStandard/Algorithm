0cbcd52081af2d9f7225f0474d475900af066e96
FakeStandard <rileylin.space@gmail.com>
2021-05-25 10:36:43 +0800

    Add Insert Sort and Merge Sort

---
+++ InsertSort/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace InsertSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 初始序列
+            int[] data = { 30, 20, 40, 50, 10 };
+
+            Console.Write("初始序列：");
+
+            foreach (var item in data)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("進行掃描...");
+            Console.WriteLine();
+
+            int i; // 掃描次數
+            int j; // 定位比較的元素
+            int tmp; // 用以暫存資料
+
+            for (i = 1; i < data.Length; i++)
+            {
+                // 將要插入的元素放置暫存變數
+                tmp = data[i];
+
+                // 已排序資料的最末端
+                j = i - 1;
+
+                while (j >= 0 && tmp < data[j])
+                {
+                    // 把所有元素往後推一格
+                    data[j + 1] = data[j];
+                    j--;
+                }
+
+
+                data[j + 1] = tmp;
+
+                // 打印每次掃描結果
+                Console.Write($"第{i}次掃描排序結果：");
+
+                foreach (var item in data)
+                {
+                    Console.Write(item + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            // 排序後的結果
+            Console.WriteLine();
+            Console.Write("排序後的序列：");
+
+            foreach (var item in data)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using static System.Console;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("請輸入陣列大小：");
+            size = int.Parse(Console.ReadLine());
+            InputArr();
+
+            Write("原始陣列：");
+            OutputArr();
+            WriteLine();
+
+            // 合併排序
+            MergeSort(arr, size);
+
+            WriteLine();
+            Write("排序結果：");
+            OutputArr();
+
+            Console.ReadLine();
+        }
+
+        static int size;
+        static int[] arr;
+
+        /// <summary>
+        /// 隨機產生陣列
+        /// </summary>
+        static void InputArr()
+        {
+            arr = new int[size + 1];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < size; i++)
+                arr[i] = Math.Abs(rand.Next(99)) + 1;
+        }
+
+        /// <summary>
+        /// 輸出陣列
+        /// </summary>
+        static void OutputArr()
+        {
+            for (int i = 0; i < size; i++)
+                Write($"{arr[i]} ");
+            WriteLine();
+        }
+
+        /// <summary>
+        /// 輸出陣列
+        /// </summary>
+        static void OutputArr(int[] arr2)
+        {
+            for (int i = 0; i < arr2.Length; i++)
+                Write($"{arr2[i]} ");
+            WriteLine();
+        }
+
+        static void MergeSort(int[] data, int size)
+        {
+            // 若長度 1，則不用排序
+            if (size <= 1)
+                return;
+
+            // 計算左邊長度
+            int Llen = size / 2;
+            int[] left = new int[Llen];
+
+            // 將原數列一一放入左邊陣列
+            for (int i = 0; i < Llen; i++)
+                left[i] = data[i];
+
+            // 計算右邊長度
+            int Rlen = size - Llen;
+            int[] right = new int[Rlen];
+
+            // 將原數列一一放入右邊陣列
+            for (int j = 0; j < Rlen; j++)
+                right[j] = data[Llen + j];
+
+            // 遞迴到每組鍵值為1
+            MergeSort(left, Llen);
+            MergeSort(right, Rlen);
+
+            // 離開遞迴後，就可進行合併排序
+            Merge(data, left, right);
+        }
+
+        static void Merge(int[] data, int[] left, int[] right)
+        {
+            int leftIndex = 0;
+            int rightIndex = 0;
+            int index = 0;
+
+            // 反覆比對
+            while ((leftIndex < left.Length) && (rightIndex < right.Length))
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    data[index] = left[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    data[index] = right[rightIndex];
+                    rightIndex++;
+                }
+
+                index++;
+            }
+
+            if (leftIndex < left.Length)
+                while (leftIndex < right.Length)
+                    data[index++] = left[leftIndex++];
+            else
+                while (rightIndex < left.Length)
+                    data[index++] = right[rightIndex++];
+
+            Write("處理過程：");
+            OutputArr(data);
+        }
+    }
+}
